6be8ddac48a8fbbf94d1551739f4f89b90ad31f8
George Sargent <george@terravigilis.com>
2021-03-29 15:02:14 -0500

    Finished 32 - Connecting Functions & Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\s your favorite animal?',
+    ];
     var materialApp = MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App, by Geo'),
+          title: Text('My First Title, by Geo'),
         ),
-        body: Text('This is my default text! Yea.'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
     return materialApp;
